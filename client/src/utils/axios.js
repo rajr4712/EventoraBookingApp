26b8c328b8ea59263041d16296579f532a2ee3d1
rajr4712 <rajr4712@gmail.com>
2026-09-03 20:27:05 +0530

    frontend api configured after render deployment

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,27 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:5000/api',
+// });
+
+// api.interceptors.request.use((config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// });
+
+// export default api;
+
+
+
+
+//after deploy your code on render this code write ciz 5000 is hardcode and it only work on local not in render --
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
